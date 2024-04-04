75fd1baf891c09f3646652496a9d9c78a9ca4185
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-04 15:27:41 +0200

    ✨ (App.jsx): add new components (Hero, AboutMe, Skills) to the App component to enhance the user interface and provide additional functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import AboutMe from "./components/AboutMe/AboutMe";
 import CanvasBackground from "./components/CanvasBackground/CanvasBackground";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <>
       <Header />
       <CanvasBackground />
+      <Hero />
+      <AboutMe />
+      <Skills />
     </>
   );
 }
