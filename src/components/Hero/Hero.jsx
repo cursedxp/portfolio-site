f7495e54cb8aa2e0a3f416d0c405affb48d5f9ca
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-04 15:26:57 +0200

    ✨ (Hero.jsx): create a new Hero component to display information about the frontend developer and UX designer 📝 (Hero.jsx): add content to the Hero component including a headline, description, and skills of the developer

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-4xl">
+        Anil <span>Frontend Developer / UX Designer</span>
+      </h2>
+      <p className="text-lg">
+        Based in Germany, I have nurtured a deep passion for crafting beautiful
+        and intuitive digital experiences. As a full-stack developer and user
+        experience designer, I specialize in React, NextJS MongoDB, Node,
+        ExpressJS, HTML, CSS, and JavaScript. My dedication to blending
+        functional technology with aesthetic design enables me to deliver
+        exceptional results for my clients.
+      </p>
+    </div>
+  );
+}
