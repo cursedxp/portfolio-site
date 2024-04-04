9a8ee54d569f19632642c2708a697fca3655e2e8
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-04 15:27:12 +0200

    ✨ (Skills.jsx): create a new component called Skills to display a list of skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+export default function Skills() {
+  return (
+    <div>
+      <ul>
+        <li>Skill Name</li>
+        <li>Skill Name</li>
+        <li>Skill Name</li>
+        <li>Skill Name</li>
+      </ul>
+    </div>
+  );
+}
