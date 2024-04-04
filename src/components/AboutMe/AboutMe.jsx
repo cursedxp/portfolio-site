8a542b918b0466b07ce13b0f748bf7b6e5991150
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-04 15:26:39 +0200

    ✨ (AboutMe.jsx): create a new component called AboutMe to display information about the author's design and development journey

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-4xl">
+        Crafting Experiences: My Design & Development Odyssey
+      </h2>
+      <p className="text-lg">
+        My path in the realm of design and development is deeply rooted in a
+        robust academic background and a steadfast dedication to the principles
+        of user-centered design. This combination has not only shaped my
+        approach but has also been instrumental in my successful involvement in
+        enhancing digital platforms for various web and mobile projects. At the
+        core of these endeavors was my commitment to improving user experiences
+        through rigorous, data-driven research and comprehensive analysis
+      </p>
+    </div>
+  );
+}
