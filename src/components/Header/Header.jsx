4eb03b40e317f7962960469e3e5b9a8f212075d8
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-06 20:42:38 +0200

    📝 (Header.jsx): update Header component to include a smaller text size for the role description ♻️ (Header.jsx): refactor Header component to use flexbox for vertical alignment of name and role description

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <header className=" z-50 fixed top-0 left-0 flex justify-between w-full p-4 text-lg">
-      <div>
+    <header className=" z-50 fixed top-0 left-0 flex justify-between w-full p-4 text-lg items-center">
+      <div className="flex flex-col">
         Anil Ozsoy
-        <span>Software Developer/UX Designer</span>
+        <span className="text-sm">Software Developer/UX Designer</span>
       </div>
       <ul className="flex space-x-4">
         <li>Home</li>
