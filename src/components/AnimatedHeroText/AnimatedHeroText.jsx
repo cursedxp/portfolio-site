a88de7436c959b226f0f646efd71f4114bffe6aa
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-06 20:43:21 +0200

    ✨ (AnimatedHeroText.jsx): create a new component for displaying animated hero text 📝 (AnimatedHeroText.jsx): add documentation to explain the purpose and usage of the component

---
+++ src/components/AnimatedHeroText/AnimatedHeroText.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
+export default function AnimatedHeroText() {
+  const el = useRef(null);
+  const typed = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: ["DEVELOPER", "UX DESIGNER"],
+      typeSpeed: 70,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    };
+  }, []);
+
+  return (
+    <div className=" text-left ml-10">
+      <span style={{ color: "#3599F6" }} ref={el} />
+    </div>
+  );
+}
