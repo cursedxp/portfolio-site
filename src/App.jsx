41c939a81186b9c302e0c1aa59ec53116d490035
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-06 20:43:59 +0200

    ✨ (App.jsx): add AnimatedHeroText component to display animated text in the hero section 📝 (App.jsx): update hero section with additional text and description to provide more information about the author

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import BallAnimation from "./components/BallAnimation/BallAnimation";
 import CanvasBackground from "./components/CanvasBackground/CanvasBackground";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import AnimatedHeroText from "./components/AnimatedHeroText/AnimatedHeroText";
 import Skills from "./components/Skills/Skills";
+
 function App() {
   return (
     <>
@@ -11,6 +13,21 @@ function App() {
       <div className="relative w-full h-screen">
         <CanvasBackground />
         <BallAnimation />
+        <div
+          className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-9xl flex flex-col  justify-center items-start gap-2 z-20"
+          style={{ color: "#F241B3" }}
+        >
+          <div>MULTI-</div>
+          <div className="ml-40">DISCIPLINED</div>
+          <AnimatedHeroText />
+          <p
+            className=" text-sm font-normal max-w-96 text"
+            style={{ color: "#3E3E3E" }}
+          >
+            Based in Germany, I have nurtured a deep passion for crafting
+            beautiful and intuitive digital experiences.
+          </p>
+        </div>
       </div>
 
       <Hero />
