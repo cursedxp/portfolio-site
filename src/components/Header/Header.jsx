cf826303e95e5e881b3854110b33204052728a36
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-08 18:47:59 +0200

    🎨 (Header.jsx): update the color of the name "Anil" to use the emerald color instead of blue for better visual contrast 🎨 (Header.jsx): update the color of the navigation links to use gray-800 instead of blue-500 for better visual contrast

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 export default function Header() {
   return (
-    <header className=" z-50 fixed top-0 left-0  w-full p-4">
-      <nav className="flex justify-between  items-center max-w-6xl mx-auto">
+    <header className=" z-50 fixed top-0 left-0  w-full p-6">
+      <nav className="flex justify-between  items-center  mx-auto">
         <div className="flex flex-col">
           <span className="text-gray-600">
             Hi there, 👋 I am
-            <span className=" font-bold text-blue-500"> Anil</span>
+            <span className=" font-bold text-emerald-500"> Anil</span>
           </span>
           <span className="text-gray-600 text-xs">
             Frontend Developer/UX Designer
           </span>
         </div>
-        <ul className="flex gap-5 text-blue-500">
+        <ul className="flex gap-5 text-gray-800">
           <li>Home</li>
           <li>Projects</li>
           <li>Blog</li>
