5470358a1dfdccdd79771d249270bce994f9c14e
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-08 15:15:27 +0200

    ✨ (Header.jsx): update header component to improve layout and add more descriptive text 📝 (Header.jsx): update header component to use semantic HTML tags and improve accessibility

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 export default function Header() {
   return (
-    <header className=" z-50 fixed top-0 left-0 flex justify-between w-full p-4 text-lg items-center">
-      <div className="flex flex-col">
-        Anil Ozsoy
-        <span className="text-sm">Software Developer/UX Designer</span>
-      </div>
-      <ul className="flex space-x-4">
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Blog</li>
-        <li>Skills</li>
-      </ul>
+    <header className=" z-50 fixed top-0 left-0  w-full p-4">
+      <nav className="flex justify-between  items-center max-w-6xl mx-auto">
+        <div className="flex flex-col">
+          <span className="text-gray-600">
+            Hi there, 👋 I am
+            <span className=" font-bold text-blue-500"> Anil</span>
+          </span>
+          <span className="text-gray-600 text-xs">
+            Frontend Developer/UX Designer
+          </span>
+        </div>
+        <ul className="flex gap-5 text-blue-500">
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Skills</li>
+        </ul>
+      </nav>
     </header>
   );
 }
