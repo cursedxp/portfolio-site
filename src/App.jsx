0d11974b4534b2b275d6eedabd6ea24ba514f0d6
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-09 15:15:28 +0200

    ✨ (App.jsx): add TechStack component to the main section of the app to display the technologies used ♻️ (App.jsx): refactor the layout of the main section to include a grid layout with borders for better visual separation of components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import AboutMe from "./components/AboutMe/AboutMe";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
-
+import TechStack from "./components/TechStack/TechStack";
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <main className="flex justify-center">
+        <div className="relative  py-16  max-w-6xl">
+          <div className="absolute inset-0 pointer-events-none border-l border-r border-gray-200"></div>
+          <div className="absolute inset-0 grid grid-cols-4 pointer-events-none -z-10">
+            <div className="col-span-1 border-dashed border-r border-gray-200"></div>
+            <div className="col-span-1 border-dashed border-r border-gray-200"></div>
+            <div className="col-span-1 border-dashed border-r border-gray-200"></div>
+          </div>
+          <AboutMe />
+          <TechStack />
+        </div>
+      </main>
 
-      <AboutMe />
       <Skills />
     </>
   );
