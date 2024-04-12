dc9265ec4830a2ccc61ae394546a5ee030784ad4
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-12 12:23:06 +0200

    📝 (Contact.jsx): update text alignment and capitalization in Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,10 @@ export default function Contact() {
   return (
     <div className="flex flex-col text-gray-800 items-center">
       <div className="p-6">
-        <h3 className="text-sm font-bold text-fuchsia-500">What is next?</h3>
-        <h1 className="text-6xl font-bold leading-tight">Get in Touch</h1>
+        <h3 className="text-sm font-bold text-fuchsia-500 text-center">
+          What is next?
+        </h3>
+        <h1 className="text-6xl font-bold leading-tight">Get in touch</h1>
       </div>
       <div className="px-6 text-base text-gray-600 max-w-3xl">
         <p className="mb-8 text-center">
