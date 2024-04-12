8d7146ae2c592e95cd0a4d8aa01de4a5749e4dd5
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-12 12:13:08 +0200

    ✨ (Contact.jsx): update Contact component UI to improve user experience and provide more information 📝 (Contact.jsx): update Contact component UI to include a heading, description, and a button to contact the user

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,24 @@
 export default function Contact() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-4xl">Contact</h2>
-      <form className="flex flex-col space-y-4">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message"></textarea>
-        <button>Send</button>
-      </form>
+    <div className="flex flex-col text-gray-800 items-center">
+      <div className="p-6">
+        <h3 className="text-sm font-bold text-fuchsia-500">What is next?</h3>
+        <h1 className="text-6xl font-bold leading-tight">Get in Touch</h1>
+      </div>
+      <div className="px-6 text-base text-gray-600 max-w-3xl">
+        <p className="mb-8 text-center">
+          Although I’m currently looking for new opportunities, my inbox is
+          always open. Whether you have a question or just want to say hi, I’ll
+          try my best to get back to you!
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <a href="mailto:anilozsoy@gmail.com">
+          <div className="bg-emerald-500 px-8 py-3 rounded-md text-white">
+            Contact me
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
