89b86f7e904ae0c5b6f777f67d919f3b2497e2ab
Anil Ozsoy <anilozsoy@gmail.com>
2024-04-15 13:55:15 +0200

    ✨ (Contact.jsx): add RevealAnimation component to enhance user experience 📝 (Contact.jsx): update text alignment and styling for better readability and aesthetics

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,5 @@
+import RevealAnimation from "../RevealAnimation/RevealAnimation";
+
 export default function Contact() {
   return (
     <div className="flex flex-col text-gray-800 items-center">
@@ -5,14 +7,16 @@ export default function Contact() {
         <h3 className="text-sm font-bold text-fuchsia-500 text-center">
           What is next?
         </h3>
-        <h1 className="text-6xl font-bold leading-tight">Get in touch</h1>
-      </div>
-      <div className="px-6 text-base text-gray-600 max-w-3xl">
-        <p className="mb-8 text-center">
-          Although I’m currently looking for new opportunities, my inbox is
-          always open. Whether you have a question or just want to say hi, I’ll
-          try my best to get back to you!
-        </p>
+        <RevealAnimation>
+          <h1 className="text-6xl font-bold leading-tight text-center">
+            Get in touch
+          </h1>
+          <p className="mb-8 px-6 text-base text-gray-600 max-w-3xl text-center">
+            Although I’m currently looking for new opportunities, my inbox is
+            always open. Whether you have a question or just want to say hi,
+            I’ll try my best to get back to you!
+          </p>
+        </RevealAnimation>
       </div>
       <div className="flex flex-col gap-4">
         <a href="mailto:anilozsoy@gmail.com">
